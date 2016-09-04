9ec54472e506b34a0146115528d5f6f8c68e4c9f
yangcaifei <yangcaifei0713@gmail.com>
2016-09-04 18:03:26 +0800

    颜色控制

---
+++ color.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+def colored(color, text):
+    table = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        # no color
+        'nc': '\033[0'
+    }
+    cv = table.get(color)
+    nc = table.get('nc')
+    return ''.join([cv, text, nc])
